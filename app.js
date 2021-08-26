925fee358243b00bcf31a0685da411f500f7e357
kou-higashi <mortalstore2011@gmail.com>
2021-08-26 17:58:18 +0900

    階乗の実装

--- app.js
+++ app.js
@@ -7,7 +7,10 @@
 */
 function factorial(n) {
   let result = 1;
-  // TODO このコメントを消して正しく実装してください。
+  let sum = 0;
+  for(let i=n;i>0;i--){
+    result = i*result;
+  }
   return result;
 }
 const assert = require('assert');
