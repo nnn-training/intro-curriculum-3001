b974c12a000b10cde1f8bce2129eb45fa82ab5a1
m-kitai <mk1tty@icloud.com>
2025-02-14 18:40:24 +0900

    総積に修正

--- app.js
+++ app.js
@@ -7,7 +7,9 @@
  */
 function factorial(n) {
   let result = 1;
-  // TODO このコメントを消して正しく実装してください。
+  for (let i = n; n > 0; n --){
+    result *= i;
+  }
   return result;
 }
 const assert = require('node:assert');
